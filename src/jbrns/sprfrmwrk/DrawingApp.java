1dc2fdda44923fd6a747d73ffef3e140acdfbb25
nelson <Qwerty123>
2016-01-12 10:11:14 +0200

    7JBSPFRW

---
+++ src/jbrns/sprfrmwrk/DrawingApp.java
@@ -0,0 +1,15 @@
+package jbrns.sprfrmwrk;
+
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class DrawingApp {
+
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("spring-config.xml");
+        Triangle triangle = (Triangle) context.getBean("triangle");
+        triangle.draw();
+    }
+
+}
