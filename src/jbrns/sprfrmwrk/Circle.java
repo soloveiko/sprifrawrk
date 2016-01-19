8871f0f435c0b9bb5c0ce151de11d1e016c0c705
nelson <Qwerty123>
2016-01-19 11:36:44 +0200

    20JBSPRFRMWRK

--- src/jbrns/sprfrmwrk/Circle.java
+++ src/jbrns/sprfrmwrk/Circle.java
@@ -1,7 +1,8 @@
 package jbrns.sprfrmwrk;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import javax.annotation.Resource;
 
 public class Circle implements Shape {
 
@@ -17,10 +18,19 @@ public class Circle implements Shape {
         return center;
     }
 
-    @Autowired
-    @Qualifier("circleRelated")
+    @Resource
     public void setCenter(Point center) {
         this.center = center;
+    }
+
+    @PostConstruct
+    public void initializeCircle() {
+        System.out.println("Init of Circle.. ");
+    }
+
+    @PreDestroy
+    public void destroyCircle() {
+        System.out.println("Destroy of Circle.. ");
     }
 
 }
--- src/jbrns/sprfrmwrk/DrawingApp.java
+++ src/jbrns/sprfrmwrk/DrawingApp.java
@@ -1,13 +1,14 @@
 package jbrns.sprfrmwrk;
 
 
-import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class DrawingApp {
 
     public static void main(String[] args) {
-        ApplicationContext context = new ClassPathXmlApplicationContext("spring-config.xml");
+        AbstractApplicationContext context = new ClassPathXmlApplicationContext("spring-config.xml");
+        context.registerShutdownHook();
         Shape shape = (Shape) context.getBean("circle");
         shape.draw();
     }
